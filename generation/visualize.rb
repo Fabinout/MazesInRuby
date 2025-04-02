e95e6ceb4e762049b123de442d227533568c50ec
Fabien Lamarque <fabien@lamarque.tech>
2025-04-02 22:16:54 +0200

    ajout de configuration de visualisation tmux

---
+++ generation/visualize.rb
@@ -0,0 +1,15 @@
+# visualize.rb
+require 'model/grid'
+require_relative 'binary_tree'
+require_relative 'image_exporter'
+require_relative 'gif_creator'
+require 'chunky_png'
+
+grid = Grid.new(20, 20)
+BinaryTree.on(grid)
+
+# Exporter l'image du labyrinthe
+png = ImageExporter.render_grid_to_png(grid)
+png.save('binary_tree_maze.png')
+system('display binary_tree_maze.png &')  # Afficher l'image (nécessite ImageMagick)
+
--- scripts/setup_tmux.sh
+++ scripts/setup_tmux.sh
@@ -4,6 +4,11 @@
 # Créer une nouvelle session Tmux nommée "maze_demo"
 tmux new-session -d -s maze_demo -n 'Maze Algorithms'
 
+# Définir le chemin racine du projet
+PROJECT_ROOT="/home/fabien-lamarque/projects/MazesInRuby"
+GENERATION_DIR="$PROJECT_ROOT/generation"
+MODEL_DIR="$PROJECT_ROOT/model"
+
 # Diviser la fenêtre en 4 panneaux
 # Layout:
 # +----------------+----------------+
@@ -29,10 +34,10 @@ tmux select-pane -t maze_demo:0.2 -T "Exécution"
 tmux select-pane -t maze_demo:0.3 -T "Visualisation"
 
 # Configurer les commandes initiales dans chaque panneau
-tmux send-keys -t maze_demo:0.0 'cd ~/maze_project && vim -O binary_tree.rb sidewinder.rb' C-m
-tmux send-keys -t maze_demo:0.1 'cd ~/maze_project && vim test_properties.rb' C-m
-tmux send-keys -t maze_demo:0.2 'cd ~/maze_project && echo "Prêt pour exécution"' C-m
-tmux send-keys -t maze_demo:0.3 'cd ~/maze_project && echo "Prêt pour visualisation"' C-m
+tmux send-keys -t maze_demo:0.0 "cd $GENERATION_DIR && vim -O sidewinder.rb binary_tree.rb" C-m
+tmux send-keys -t maze_demo:0.1 "cd $PROJECT_ROOT && vim test_properties.rb" C-m
+tmux send-keys -t maze_demo:0.2 "cd $PROJECT_ROOT && echo 'Prêt pour exécution'" C-m
+tmux send-keys -t maze_demo:0.3 "cd $PROJECT_ROOT && echo 'Prêt pour visualisation'" C-m
 
 # Sélectionner le premier panneau
 tmux select-pane -t maze_demo:0.0
