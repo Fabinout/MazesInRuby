d46e32671929c822bf186b7c8873a629ce910010
Fabien Lamarque <fabien@lamarque.tech>
2025-07-14 22:15:48 +0200

    Mise à jour du projet pour lire et écrire des évènements de génération de labyrinthe

--- model/grid.rb
+++ model/grid.rb
@@ -1,79 +1,71 @@
-require 'model/cell'
+require_relative 'maze_event_base'
+require_relative 'maze_event_handlers'
+require_relative 'cell'
+require 'json'
 
-# grille
 class Grid
-  attr_reader :rows, :columns
+  attr_reader :rows, :columns, :events
 
   def initialize(rows, columns)
-    @rows = rows
-    @columns = columns
-    @grid = prepare_grid
-    configure_cells
+    @events = []
+    apply_event(GridCreatedEvent.new(rows, columns))
   end
 
-  def prepare_grid
-    Array.new(rows) do |row|
-      Array.new(columns) do |column|
-        Cell.new(row, column)
+  private def apply_event(event)
+    @events << event
+    event.apply(self)
+  end
+
+  def create_grid(rows, columns)
+    @rows = rows
+    @columns = columns
+    @grid = Array.new(rows) do |row|
+      Array.new(columns) do |col|
+        Cell.new(row, col)
+      end
+    end
+
+    # Configuration des cellules voisines
+    @grid.each_with_index do |row, r|
+      row.each_with_index do |cell, c|
+        cell.north = @grid[r-1][c] if r > 0
+        cell.south = @grid[r+1][c] if r + 1 < rows
+        cell.west = @grid[r][c-1] if c > 0
+        cell.east = @grid[r][c+1] if c + 1 < columns
       end
     end
   end
 
-  def configure_cells
-    each_cell do |cell|
-      row, col = cell.row, cell.column
-      cell.north = self[row - 1, col]
-      cell.south = self[row + 1, col]
-      cell.east = self[row, col + 1]
-      cell.west = self[row, col - 1]
-    end
-  end
-
-  def [](row, column)
-    return nil unless row.between?(0, rows - 1) && column.between?(0, columns - 1)
+  def cell_at(row, column)
     @grid[row][column]
   end
 
-  def random_cell
-    row = rand(@rows)
-    column = rand(@grid[row].length)
-    self[row, column]
+  def link_cells(from_row, from_column, to_row, to_column)
+    apply_event(CellsLinkedEvent.new(from_row, from_column, to_row, to_column))
   end
 
-  def size
-    @rows * @columns
+  def save_events(filename)
+    File.write(filename, JSON.pretty_generate(@events.map(&:to_h)))
   end
 
-  def each_row(&block)
-    @grid.each(&block)
-  end
+  def self.replay_from_file(filename)
+    events = JSON.parse(File.read(filename))
+    grid = nil
 
-  def each_cell
-    @grid.each do |row|
-      row.each do |cell|
-        yield cell if cell
-      end
+    events.each do |event_data|
+      event = case event_data['type']
+              when 'GridCreatedEvent'
+                grid = new(event_data['rows'], event_data['columns'])
+              when 'CellsLinkedEvent'
+                grid.link_cells(
+                  event_data['from_row'],
+                  event_data['from_column'],
+                  event_data['to_row'],
+                  event_data['to_column']
+                )
+              end
     end
-  end
 
-  def to_s
-    output = "+" + "---+" * columns + "\n"
-    each_row do |row|
-      top = "|"
-      bottom = "+"
-
-      row.each do |cell|
-        cell = Cell.new(-1, -1) unless cell
-        body = "   "
-        east_boundary = (cell.linked?(cell.east) ? " " : "|")
-        top << body << east_boundary
-        south_boundary = (cell.linked?(cell.south) ? "   " : "---")
-        corner = "+"
-        bottom << south_boundary << corner
-      end
-      output << top << "\n"
-      output << bottom << "\n"
-    end
-    output
+    grid
   end
 end
