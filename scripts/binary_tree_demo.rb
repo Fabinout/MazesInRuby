44d7d490d794a9a0ad640c6a9e79f5706ef8107e
Fabien Lamarque <fabien-externe.lamarque@edf.fr>
2025-08-28 13:16:32 +0200

    Mise à jour du script d'initialisation TMUX

--- scripts/binary_tree_demo.rb
+++ scripts/binary_tree_demo.rb
@@ -1,9 +1,35 @@
 # frozen_string_literal: true
 
-require 'model/grid'
-require 'generation/binary_tree'
+require_relative '../model/grid'
+require_relative '../generation/binary_tree'
 
-grid = Grid.new(10, 10)
+# Interprétation des arguments:
+# - 0 arg  => 8x8 (par défaut)
+# - 1 arg  => x x (carrée)
+# - 2 args => x y (lignes, colonnes)
+rows, cols = case ARGV.length
+             when 0
+               [8, 8]
+             when 1
+               begin
+                 x = Integer(ARGV[0], 10)
+                 raise ArgumentError if x <= 0
+                 [x, x]
+               rescue ArgumentError
+                 abort "Usage: ruby binary_tree_demo.rb [taille] ou ruby binary_tree_demo.rb [lignes colonnes]\nExemples: ruby binary_tree_demo.rb 10   |   ruby binary_tree_demo.rb 20 30"
+               end
+             else
+               begin
+                 x = Integer(ARGV[0], 10)
+                 y = Integer(ARGV[1], 10)
+                 raise ArgumentError if x <= 0 || y <= 0
+                 [x, y]
+               rescue ArgumentError
+                 abort "Usage: ruby binary_tree_demo.rb [taille] ou ruby binary_tree_demo.rb [lignes colonnes]\nExemples: ruby binary_tree_demo.rb 10   |   ruby binary_tree_demo.rb 20 30"
+               end
+             end
+
+grid = Grid.new(rows,cols)
 BinaryTree.on(grid)
 
 puts grid
