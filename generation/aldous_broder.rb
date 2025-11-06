533a9c00b1e642184645d1563fe6edb45330971f
Fabien Lamarque <fabien@lamarque.tech>
2025-11-06 21:40:30 +0100

    step 5

--- generation/aldous_broder.rb
+++ generation/aldous_broder.rb
@@ -3,7 +3,15 @@ require_relative '../model/grid'
 
 class AldousBroder
   def self.on(grid)
-    grid
-
+    cell = grid.random_cell
+    unvisited = (grid.rows * grid.columns) - 1
+    until unvisited.zero?
+      neighbor = cell.neighbors.sample
+      if neighbor.links.empty?
+        cell.link(neighbor)
+        unvisited -= 1
+      end
+      cell = neighbor
+    end
   end
 end
