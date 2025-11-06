699b58f5a1e73ad66037496f8c41ad9f44909aba
Fabien Lamarque <fabien@lamarque.tech>
2025-11-06 21:31:44 +0100

    step 3

--- generation/sidewinder.rb
+++ generation/sidewinder.rb
@@ -3,7 +3,24 @@
 class Sidewinder
 
   def self.on(grid)
+    grid.each_row do |row|
+      run = []
+      row.each do |cell|
+        run << cell
+        at_eastern_boundary = cell.east.nil?
+        at_northern_boundary = cell.north.nil?
 
-    grid
+        should_close_out = at_eastern_boundary || (rand(2) == 0 && !at_northern_boundary)
+
+        if should_close_out
+          member = run.sample
+          member.link(member.north)
+          run.clear
+        else
+          cell.link(cell.east)
+        end
+      end
+      grid
+    end
   end
 end
